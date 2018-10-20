c925118b24e88e0ede623588220c18f74fe4f97c
Artem Rebryk <artemrebrik8099@gmail.com>
2018-10-20 08:58:50 +0300

    create anagramm

---
+++ 03_anagram.js
@@ -0,0 +1,9 @@
+const testData1 = ["стационар", "соратница"];
+const testData2 = ['покраснение', 'пенсионерка'];
+
+function isAnagram(str1, str2) {
+
+}
+
+console.log(isAnagram(testData1[0], testData1[1])) //true
+console.log(isAnagram(testData2[0], testData2[1])) //true
